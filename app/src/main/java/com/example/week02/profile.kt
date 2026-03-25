a0fac78dd48a0bd9e1e3f7ec2e8d9522c497f876
jyoun0330 <yeyoun714930@gmail.com>
2026-03-26 01:12:29 +0900

    Revert "[sunhaeng] feat: 2주차 과제 제출"
    This reverts commit 6594312fdbb3f87996024cf1c54fe312eec601ed.

--- app/src/main/java/com/example/week02/profile.kt
+++
@@ -1,5 +0,0 @@
-package com.example.week02
-
-import androidx.fragment.app.Fragment
-
-class ProfileFragment : Fragment(R.layout.fragment_profile)
--- app/src/main/java/com/example/week02/wishlist.kt
+++
@@ -1,5 +0,0 @@
-package com.example.week02
-
-import androidx.fragment.app.Fragment
-
-class WishlistFragment : Fragment(R.layout.fragment_wishlist)
